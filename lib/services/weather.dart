1c9defa2f7ce6fd589f06c9174cc28dd6b47f9ca
luanapettersm <luanamarchieviskz@gmail.com>
2022-10-26 21:51:58 -0300

    Update do arquivo co o novo desafio proposto

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,3 +1,9 @@
+import 'package:tempo_template/services/location.dart';
+import 'package:tempo_template/services/networking.dart';
+
+const apiKey = '84b7030cb2f2c1f48914a22cd312277f';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
   String getWeatherIcon(int condition) {
     if (condition < 300) {
@@ -29,5 +35,17 @@ class WeatherModel {
     } else {
       return 'Leve um 🧥';
     }
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}'
+            '&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
   }
 }
